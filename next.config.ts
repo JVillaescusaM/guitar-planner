5d6f5d2a5e38b23a5019ef33536d1d1c42a850f7
Juan Villaescusa <VillaescusaMJuan@gmail.com>
2026-05-19 00:01:50 +0200

    versión config ts 2

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,16 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        // Aplicamos la regla a absolutamente todas las páginas de la web
         source: "/(.*)",
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups", // 👈 La directiva mágica anti-bloqueos
+            value: "unsafe-none", // Apaga el bloqueo de la ventana emergente
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none", // Apaga el bloqueo de comunicación con Google
           },
         ],
       },
